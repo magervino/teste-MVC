4d80267c33f18a0ecfc4dd688ff0e2941402fabd
Mariana Gervino <marianatrevisoli@bol.com.br>
2016-06-22 14:51:56 -0300

    Testing Asp.Net MVC with SQL Server Compact

---
+++ MvcDemo1/Models/MovieDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcDemo1.Models
+{
+    public class MovieDB
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string director { get; set; }
+        public DateTime date { get; set; }
+    }
+
+    public class MovieDBContext : DbContext {
+        public DbSet<MovieDB> Movies { get; set; }
+    }
+}
